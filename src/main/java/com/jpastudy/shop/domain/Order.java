3a7b82e26ffcd30dd89c6ce2b8e347de07a1a3d8
seungdols <seungdols0822@gmail.com>
2022-08-23 09:21:20 +0900

    Modify class

--- src/main/java/com/jpastudy/shop/domain/Order.java
+++ src/main/java/com/jpastudy/shop/domain/Order.java
@@ -10,9 +10,13 @@ public class Order {
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
     private LocalDateTime orderDate;
+
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
@@ -27,12 +31,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
